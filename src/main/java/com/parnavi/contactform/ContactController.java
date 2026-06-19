198c96f1e64dc334843f5e321e8f71702c428ac0
parnavvii <parnavisharma311@gmail.com>
2026-06-20 03:47:14 +0530

    Task 2

--- src/main/java/com/parnavi/contactform/ContactController.java
+++ src/main/java/com/parnavi/contactform/ContactController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class contactController {
+public class ContactController {
 
     @Autowired
     private ContactRepository repository;
